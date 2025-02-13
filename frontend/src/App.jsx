abf2bd3896be6c01f44468062e6f372468ab3dd9
Tejashwini Bhendarkar <tmb22052005@gmail.com>
2025-02-13 21:00:57 +0530

    create app.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./landing_page/home/HomePage";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
