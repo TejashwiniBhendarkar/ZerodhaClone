8692bcdabd2366ad928b51e485f47da371885ea0
Tejashwini Bhendarkar <tmb22052005@gmail.com>
2025-02-13 16:44:53 +0530

    info added

---
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Brokerage from './Brokerage';
+import Footer from '../Footer';
+import Hero from './Hero';
+function PricingPage() {
+    return ( 
+       <>
+        <Navbar />
+        < Hero />
+        <Brokerage />
+        <Footer /> 
+       </>
+     );
+}
+
+export default PricingPage;
